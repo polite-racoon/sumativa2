be201abad7f54cd6fe3760ce8a0c246e7d3c76a3
Lyoaw <pieromito96@gmail.com>
2024-11-14 12:36:28 -0300

    Atributos simples y prueba si funciona el push

--- src/com/mycompany/applibreria/Docente.java
+++ src/com/mycompany/applibreria/Docente.java
@@ -1,6 +1,11 @@
 package com.mycompany.applibreria;
 
 public class Docente extends Usuario {
+
+    public String Nombre;
+    public String Titulo;
+    public boolean Genero;
+
     public Docente(String RUN) {
         super(RUN);
     }
--- src/com/mycompany/applibreria/Estudiante.java
+++ src/com/mycompany/applibreria/Estudiante.java
@@ -2,6 +2,9 @@ package com.mycompany.applibreria;
 
 public class Estudiante extends Usuario {
     // GENERAMOS DATOS DE USUARIOS BASE
+    public String Nombre;
+    public String Carrera;
+    public boolean Genero;
 
     public Estudiante(String RUN) {
         super(RUN);
